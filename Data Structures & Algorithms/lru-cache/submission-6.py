6fe5dbf2339dec044e2ca41dea68427a3c2f7aef
Sahil Sanjay Gujral <73101338+ZackAboy@users.noreply.github.com>
2026-04-20 16:24:26 -0700

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.py
@@ -0,0 +1,66 @@
+class ListNode:
+    def __init__(self, val = None, next = None, prev = None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = defaultdict(int)
+
+        self.least = ListNode(-1)
+        self.most = ListNode(-1)
+
+        self.least.prev = None
+        self.least.next = self.most
+
+        self.most.prev = self.least
+        self.most.next = None
+
+        self.cap = capacity
+
+    def add(self, node):
+        old = self.most.prev
+        old.next = node
+
+        node.next = self.most
+        self.most.prev = node
+
+        node.prev = old
+
+    def rem(self, node):
+        prev = node.prev
+        nex = node.next
+
+        prev.next = nex
+        nex.prev = prev
+
+        node.next = None
+        node.prev = None
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self.rem(node)
+            self.add(node)
+            return node.val[1]
+
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            self.rem(node)
+            del self.cache[key]
+
+        node = ListNode((key,value))
+        self.cache[key] = node
+        self.add(node)
+
+        if len(self.cache) > self.cap:
+            node = self.least.next
+            key, val = node.val
+            del self.cache[key]
+            self.rem(node)
