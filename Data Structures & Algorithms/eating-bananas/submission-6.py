bf222dad464ff952eefec7a3d72144e4504accc6
Sahil Sanjay Gujral <73101338+ZackAboy@users.noreply.github.com>
2026-05-08 12:48:52 -0700

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        def bananer(x):
+            ans = 0
+            for val in piles:
+                ans += math.ceil(val/x)
+            return ans
+
+        l = 1
+        r = max(piles)
+        res = float('inf')
+
+        while l<=r:
+            mid = (l+r)//2
+
+            if bananer(mid) > h:
+                l = mid + 1
+
+            elif bananer(mid) <=h :
+                res = min(res, mid)
+                r = mid - 1
+
+        return res
